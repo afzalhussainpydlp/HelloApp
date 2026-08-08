b8b9ba153366b37180b8a724c9589b2e604a47df
Afzal Hussain <ah8292700232@gmail.com>
2026-08-08 22:07:56 +0530

    add msg sent sysytem

---
+++ static/js/chat.js
@@ -0,0 +1,20 @@
+function sendMessage() {
+
+    let input = document.getElementById("messageInput");
+    let message = input.value;
+
+    if (message.trim() === "") {
+        return;
+    }
+
+    let messages = document.getElementById("messages");
+
+    let newMessage = document.createElement("div");
+
+    newMessage.className = "sent";
+    newMessage.innerText = message;
+
+    messages.appendChild(newMessage);
+
+    input.value = "";
+}
